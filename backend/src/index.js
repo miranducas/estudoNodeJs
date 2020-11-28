fc92596dcf734d243bf8d59a8157ecc6ba053c15
miranducas <lucasgm30@gmail.com>
2020-11-28 15:55:52 -0300

    Backend app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const routes = require('./routes')
+const cors = require('cors'); //módulo de segurança. Determina quem poderá acessar a aplicação
+const routes = require('./routes');
 const app = express();
 
+
+app.use(cors);
 app.use(express.json()); 
 app.use(routes);
 
